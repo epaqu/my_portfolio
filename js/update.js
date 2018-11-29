bfb0705ab29b8c01a9b350886cb011bf524e5bc9
Jaewoo <epaqu.kaist.ac.kr>
2018-11-29 14:11:28 +0900

    version 1.0

--- js/update.js
+++ js/update.js
@@ -1,5 +1,5 @@
 function lastUpdate(){
-    var m = "This portolio was last updated on: " + document.lastModified;
+    var m = "Last update: " + document.lastModified;
     var p = m.length-8;
     document.writeln("<center>");
     document.write(m.substring(p, 0));
